fa78221c9cbd3903375706e8d53535b30ad1e5e7
talafarael <127351345+talafarael@users.noreply.github.com>
2023-08-18 19:19:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,31 +3,33 @@ const path = require('path');
 const authRouter = require('./authRouter');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const cookie=require('js-cookie')
+const cookie = require('js-cookie');
+const axios = require('axios');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 2000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, 'ejs'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use('/auth', authRouter);
-app.use(express.static(path.join(__dirname, "ejs")))
+app.use(express.static(path.join(__dirname, 'ejs')));
+
 const start = async () => {
     try {
-
         await mongoose.connect(
             'mongodb+srv://shine:shine1234@cluster0.o8mqsuq.mongodb.net/'
         );
-        app.get('/', (req, res) => {
+        app.get('/index', (req, res) => {
             res.render('index');
-          });
-
+        });
+        app.get('/', async (req, res) => {
+            res.render('notice.ejs');
+        });
         app.listen(PORT, () => {
             console.log(PORT);
-         
         });
     } catch (e) {
         console.log(e);
