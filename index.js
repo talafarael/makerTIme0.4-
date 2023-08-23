203b42810f2270624bf8dc9fc45ab7b2c541369f
talafarael <127351345+talafarael@users.noreply.github.com>
2023-08-23 16:53:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,22 @@ const path = require('path');
 const authRouter = require('./authRouter');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const cookie = require('js-cookie');
-const axios = require('axios');
+const swaggerSpecs = require('./swaggerDOC');
+const swaggerUi = require('swagger-ui-express');
 const app = express();
+
 const PORT = process.env.PORT || 2000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, 'ejs'));
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/auth', authRouter);
 app.use(express.static(path.join(__dirname, 'ejs')));
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 const start = async () => {
     try {
         await mongoose.connect(
