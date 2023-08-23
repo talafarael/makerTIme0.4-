e347118760ec6f25d3e8421382dd95d5d4702869
talafarael <127351345+talafarael@users.noreply.github.com>
2023-08-23 16:53:02 +0300

    Update authRouter.js

--- authRouter.js
+++ authRouter.js
@@ -4,11 +4,16 @@ const {check}=require('express-validator')
 const controller=require('./authController')
 const authMiddleware=require('./userid')
 const middaleware=require('./middaleware/authMiddleware')
+
+
+
 router.post('/registration', [
     check('username', "Имя пользователя не может быть пустым").notEmpty(),
     check('password', "Пароль должен быть больше 4 и меньше 10 символов").isLength({min:4, max:10})
 ],controller.registration)
+
 router.post('/login', controller.login)
+
 router.get('/users',middaleware, controller.getUsers)
 router.post('/notice', controller.notice)
 router.get('/creatnotice',authMiddleware, controller.creatnotice)
