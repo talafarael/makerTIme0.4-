f901325247f9fef66932b1df547e10743d340293
talafarael <127351345+talafarael@users.noreply.github.com>
2023-08-23 16:53:15 +0300

    Create swaggerDOC.js

---
+++ swaggerDOC.js
@@ -0,0 +1,22 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'My API',
+      version: '1.0.0',
+      description: 'Описание вашего API',
+    },
+    servers: [
+      {
+        url: 'http://localhost:2000', // Укажите базовый URL вашего API
+      },
+    ],
+  },
+  apis: ['./swaggeschema.js'], 
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = specs;
