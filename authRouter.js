63cc9953b76c4531e42c96bd3c87c0d6a8d8e792
talafarael <127351345+talafarael@users.noreply.github.com>
2023-08-29 20:51:26 +0300

    Update authRouter.js

--- authRouter.js
+++ authRouter.js
@@ -12,8 +12,9 @@ router.post('/registration', [
     check('password', "Пароль должен быть больше 4 и меньше 10 символов").isLength({min:4, max:10})
 ],controller.registration)
 
+router.post('/registerchaeck', controller.registerchaeck)
 router.post('/login', controller.login)
-
+router.post('/emailsend', controller.sendemail)
 router.get('/users',middaleware, controller.getUsers)
 router.post('/notice', controller.notice)
 router.get('/creatnotice',authMiddleware, controller.creatnotice)
