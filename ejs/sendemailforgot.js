087f013f197bbdd2f26202d2c56b5b0c731eaa66
talafarael <127351345+talafarael@users.noreply.github.com>
2023-09-17 17:01:51 +0300

    Update sendemailforgot.js

--- ejs/sendemailforgot.js
+++ ejs/sendemailforgot.js
@@ -1,14 +1,20 @@
+
+
 const sendlink=document.querySelector('#resend-pass')
 sendlink.addEventListener('submit',async (event)=>{
     event.preventDefault();
     
    
     const email = event.target.elements.emailsend.value;
-    await fetch("/auth/sendemailforgot", {
+    try{
+await fetch("/auth/sendemailforgot", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify({ email}),
     });
+ 
+ }catch{}  
+    
 })
