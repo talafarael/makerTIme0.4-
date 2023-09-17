b7f3538b3bf63d02b8e9f0596578f3ce050e1a71
talafarael <127351345+talafarael@users.noreply.github.com>
2023-09-17 17:01:49 +0300

    Update acountforgot.js

--- ejs/acountforgot.js
+++ ejs/acountforgot.js
@@ -5,19 +5,23 @@ resetpass.addEventListener('submit', async (event) => {
 
     
     const password  = event.target.elements.password.value;
-
+    const urlkey=window.location.search
+    const url=new URLSearchParams(urlkey)             
+   const tokenurl=url.get('token')
     try {
         const response = await fetch('/auth/resetpassword', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ password }),
+            body: JSON.stringify({ password ,tokenurl}),
         });
 
        
         if (response.ok) {
-     
+            if(!response.ok){}
+            console.log('fafa')
+            
             console.log('Password reset successful');
         } else {
           
